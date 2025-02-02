10c6d572ccda7e1a387e8b1a74ca6ddc988ed2e1
PaolaMishell <paolatenecela1@gmail.com>
2025-02-01 23:24:08 -0500

    Agregar carpeta para las solicitudes HTTP

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+export const getAutoRecommendation = async (texto) => {
+  const url = 'http://192.168.0.3:8000/recomendar';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ texto }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Error al obtener respuesta del chatbot');
+  }
+
+  return response.json();
+};
